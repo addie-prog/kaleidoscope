1ad4561c4043ca2e54d2464d19d8f3e59f6f0932
tbidev <accesstbilq@gmail.com>
2025-12-12 11:33:01 +0530

    made changes in allocate budget

--- app/budget-tool/page.tsx
+++ app/budget-tool/page.tsx
@@ -39,7 +39,7 @@ export default function BudgetTool() {
         />
       )}
 
-      {step === 3 && <ReportStep onNext={(value: number) => setStep(value)} />}
+      {step === 3 && <ReportStep  />}
     </div>
   );
 }
