9d4f9731de0916e1d535fe950e475e38ad4019b8
accesstbilq <accesstbilq@gmail.com>
2026-01-08 19:06:30 +0530

    made changes in tab name

--- src/app/api/user-session/store-report/route.ts
+++ src/app/api/user-session/store-report/route.ts
@@ -1,12 +1,39 @@
 import { adminDb } from "@/lib/firebase-auth";
+import { Timestamp } from "firebase-admin/firestore";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  const snap = await adminDb.collection("reports").get();
-  const data = snap.docs.map(doc => ({
-    id: doc.id,
-    ...doc.data(),
-  }));
+export async function POST(request: Request) {
+  const { reportName, sessionId, techType, initialBudget, projectName, status, email } = await request.json()
+  try {
+    let docRef: any;
 
-  return NextResponse.json(data);
+    const fields: any = {
+      "Session ID": sessionId,
+      "Report Name": reportName ?? null,
+      "Date Created": Timestamp.now(),
+      "Initial Budget": initialBudget,
+      "Project Name": projectName ?? null,
+      "Status": status,
+      "Tech Type": techType ?? null,
+      "Email": email ?? null
+    }
+    docRef = await adminDb
+      .collection("reports")
+      .add({
+        ...fields
+      });
+
+
+    return NextResponse.json({
+      success: true,
+      id: docRef.id,
+    });
+
+
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.message || "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
